a099e0150361dde40a3f9d6c4b5bd965415abfcb
GalinaUzunova <gbuzunova13@gmail.com>
2025-10-09 12:28:02 +0200

    refactor:client adn card payment methods

--- src/main/java/org/ecospace/model/dto/SubscriptionDtos.java
+++ src/main/java/org/ecospace/model/dto/SubscriptionDtos.java
@@ -1,8 +1,7 @@
 package org.ecospace.model.dto;
 
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
